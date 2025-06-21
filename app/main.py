32c723aa663e48a84e836a682cee4414850bf632
Roshan <roshanerukulla05@gmail.com>
2025-06-21 01:12:53 -0400

    U

--- app/main.py
+++ app/main.py
@@ -50,3 +50,6 @@ app.include_router(onboarding.router, prefix="/api/v1")
 app.include_router(login.router, prefix="/api/v1")
 app.include_router(user.router, prefix="/api/v1")
 app.include_router(query.router, prefix="/api/v1")
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8080)
