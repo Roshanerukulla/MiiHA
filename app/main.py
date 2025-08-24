f36ba21fdb04363e333e246549af824cae56785e
Roshan <roshanerukulla05@gmail.com>
2025-08-24 17:58:00 -0400

    updated the cors origin

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 # ✅ CORS middleware (update allowed origins if needed)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # ⬅️ You can change to ["https://your-flutter.web.app"] later
+    allow_origins=["https://mihachat.storage.googleapis.com/index.html"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
