e2e30489dc4bc615ec316699d0c82b0e944ff1cd
Roshan <roshanerukulla05@gmail.com>
2025-09-09 17:39:40 -0400

    port number

--- app/main.py
+++ app/main.py
@@ -61,4 +61,5 @@ app.include_router(chat_session.router, prefix="/api/v1/chat", tags=["Chat"])
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("app.main:app", host="0.0.0.0", port=port)
